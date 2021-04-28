c97fed1ffccf3411c33fa828eeab044707db2ac7
Thiago Gouvêa <44646118+thiagosng@users.noreply.github.com>
2021-04-28 16:09:27 -0300

    Questão 07 Prova IA

--- index.js
+++ index.js
@@ -1,91 +1,156 @@
-function random(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-  
-    // The maximum is exclusive and the minimum is inclusive
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-//gerar a populacao de 0 a 300
-// verificar o individuo com a formula
-// converter o resultado em binario
-//  f(x) = x2 - 4x + 6 
-/**
- *   mutate(mutationRate) {
-    for (let i = 0; i < this.keys.length; i += 1) {
-      // If below predefined mutation rate,
-      // generate a new random letter on this position.
-      if (Math.random() < mutationRate) {
-        this.keys[i] = generateLetter();
-      }
-    }
-  }
- */
-//fazer o cruzamento
-
-function generatePopulation() {
-  let population = []
-
-  for (let index = 0; index < 8; index++) {
-      population.push(random(0, 300));
-  }
-
-  return population;
-}
-
-function calculate (value, b, c) {
-  return Math.floor(Math.pow(value, 2) - b * value + c);
-}
-
-function getIndividualsAsBinary() {
-    const newArray = generatePopulation();
-    const resultArray = [];
-
-    newArray.map((item) => {
-      let value = item.toString(2);
+let shuffle = (this && this.shuffle) || function () {
+  for (var v = 0, i = 0, ag = arguments.length; i < ag; i++) v = v + arguments[i].length;  
+    for (var r = Array(v), k = 0, i = 0; i < ag; i++)
+      for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++) r[k] = a[j];
       
-      console.log(value.length);
+  return r;
+};
+//("");
 
-        if(value.length < 12) {
-          while(value.length < 12) {
-              value = 0 + value;
-          }
-      }
-      resultArray.push(value.toString(2));
+let individuoPop = [
+{ 
+  Individuo: "10010101", 
+  fitness: 0 
+},
+{ 
+  Individuo: "11011010", 
+  fitness: 0 
+},
+{ 
+  Individuo: "01101011", 
+  fitness: 0 
+},
+{ 
+  Individuo: "01101101", 
+  fitness: 0 
+},
+{ 
+  Individuo: "11101110", 
+  fitness: 0 
+},
+{ 
+  Individuo: "01111010", 
+  fitness: 0 
+},
+{ 
+  Individuo: "11000111", 
+  fitness: 0 
+},
+{ 
+  Individuo: "11100001", 
+  fitness: 0 
+},
+];
+
+
+function encontrarFitness(x) {
+
+formulaCalc = parseFloat((Math.pow(x, 2) - 4 * x + 6).toFixed(10));
+return formulaCalc;
+
+}
+
+function algoritmoGenetico(populacao) {
+  let individuoPop = populacao;
+  let geracoes = 0;
+  let encontrar = false;
+
+  let novaGeracao = function () {
+  geracoes++;
+
+  console.log("População: ", individuoPop);
+  console.log("Geração: ", geracoes);
+
+  let populacaoFit = individuoPop.map(function (expr) {
+   
+      let valorDecimal = parseInt(expr.Individuo, 2);
+ 
+      let resultadoFit = encontrarFitness(valorDecimal);
+    return {
+      Individuo: expr.Individuo,
+      fitness: resultadoFit,
+    };
+  });
+
+  console.log("Fitness da população: ", populacaoFit);
+
+ 
+  let encontrarIndividuo = populacaoFit.filter(function (i) {
+    return i.fitness < 200;
+  });
+
+  console.log("encontrarIndividuo", encontrarIndividuo);
+
+  if (encontrarIndividuo.length > 0) {
+    console.log( "Individuos menos aptos : ", encontrarIndividuo);
+    console.log("Geração: ", geracoes);
+    encontrar = true;
+  }
+
+  
+  let ordenarPopulacao = populacaoFit.sort(function (IndividuoA, IndividuoB){
+    if (IndividuoA.fitness > IndividuoB.fitness) {
+      return -1;
+    }
+    if (IndividuoA.fitness < IndividuoB.fitness) {
+      return 1;
+    }
+    return 0;
+  });
+
+  console.log(
+    "População ordenada com base no fitness: ",
+    ordenarPopulacao
+  );
+
+  let length = ordenarPopulacao.length;
+
+  
+  let melhoresIndividuos = ordenarPopulacao.slice(0, length / 2);
+
+  let firstArray = [];
+  let segundoArray= [];
+
+ 
+  melhoresIndividuos.forEach(function (expr) {
+      let binario = expr.Individuo;
+      let first = binario.slice(0, 4); //separar os arrays
+      let second= binario.slice(4, 8); //separar os arrays
+    firstArray.push(first);
+    segundoArray.push(second);
+  });
+
+  
+  segundoArray.reverse();
+
+  let novaPopulacao = firstArray.map(function (parcial, i) {
+    return {
+      Individuo: "" + parcial + segundoArray[i],
+      fitness: 0,
+    };
+  });
+
+  individuoPop = shuffle(melhoresIndividuos, novaPopulacao);
+
+  let populacaoEvoluida = individuoPop.map(function (expr) {
+   
+      let dados = expr.Individuo.split("");
+      let modDadosArray = dados.map(function (Individuo) {
+      return "" + Math.round(Math.random());
     });
-
-    return resultArray;
-}
-
-function convertFromBinaryToDecimal(array) {
-  const newArray = [];
-
-  array.map((item) => {
-    newArray.push(parseInt(item, 2));
+    let modDados = modDadosArray.join("");
+    return {
+      Individuo: modDados,
+      fitness: 0,
+    };
   });
+  individuoPop = populacaoEvoluida;
+};
 
-  return newArray;
+while (geracoes < 200) {
+  novaGeracao();
 }
 
-function calculateEachMember() {
-  const binaries = getIndividualsAsBinary();
-  const newArray = [];
-
-  const 
-
-  // [000111,0111,0111,0111]
-
-  // [{ binary: 000111, decimal: 0, result: 0 }, { binary: 00101, decimal: 0, result}]
-
-  // 
-
-
-
-  binaries.map((item) => {
-    newArray.push(calculate(parseInt(item, 2), 4, 6));
-  });
-
-  return newArray;
+return "População encontrada com os critérios";
 }
-
-console.log(calculateEachMember());
+algoritmoGenetico(individuoPop);
